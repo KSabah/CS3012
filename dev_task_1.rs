e7940fd59b5f40e47b9f1b46383a68fddd9b333d
KSabah <khans3@tcd.ie>
2018-09-28 16:52:05 +0100

    Initial code for binary tree

--- dev_task_1.rs
+++ dev_task_1.rs
@@ -1,6 +1,81 @@
 /*  Written by Sabah Khan, khans3@tcd.ie, 16320187
-    Last modified at: 13:23, 28/09/2018
+    Last modified at: , 28/09/2018
 */
+mod LCA_algorithm
+{
+    use std::fmt;
+
+    #[derive(Debug)]
+    struct Tree<T>
+    where T: Ord{
+        root: Option<Box<Node<T>>>,}
+
+    #[derive(Debug)]
+    struct Node<T>
+    where T: Ord
+    {
+        value: T,
+        left: Option<Box<Node<T>>>,
+        right: Option<Box<Node<T>>>,
+    }
+
+    macro_rules! insert_at
+    {
+        ($node:expr, $value:expr) => {
+            {
+                (move || {
+                    if let Some(ref mut node) = $node.as_mut() {
+                        node.insert($value);
+                        return
+                    }
+                    $node = Some(Box::new(Node::new($value)));
+                })()
+            }
+        };
+    }
+
+    macro_rules! fmt_node
+    {
+        ($fmt:expr, $node:expr) => {
+            if let Some(ref node) = $node.as_ref() {
+                try!(node.fmt($fmt));
+            }
+        };
+    }
+
+    impl<T>Tree<T>
+    where T: Ord
+    {
+        fn new() -> Self {
+            Tree {
+                root: None,
+            }
+        }
+        fn insert(&mut self, value: T) {
+            insert_at!(self.root, value);
+        }
+    }
+
+    impl<T>Node<T>
+    where T: Ord
+    {
+        fn new(value: T) -> Self {
+            Node
+            {
+                value: value,
+                left: None,
+                right: None,
+            }
+        }
+        fn insert(&mut self, value: T)
+        {
+            if value <= self.value
+                insert_at!(self.left, value);
+            else
+                insert_at!(self.right, value);
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests
