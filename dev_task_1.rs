00801edf9b502ae377ded4c35b138c7081948204
KSabah <khans3@tcd.ie>
2018-09-30 18:53:09 +0100

    Modified entire project so all functions should be operating now!

--- dev_task_1.rs
+++ dev_task_1.rs
@@ -1,35 +1,31 @@
 #[derive(Debug)]
-pub struct Tree{
+pub struct Tree {
     pub root: Option<Box<Node>>,
 }
 
-#[derive(Debug)]
-pub struct Node{
+#[derive(Debug, Clone)]
+pub struct Node {
     pub value: i32,
     pub left: Option<Box<Node>>,
     pub right: Option<Box<Node>>,
 }
 
 macro_rules! insert_at {
-    ($node:expr, $value:expr) => {
-        {
-            (move || {
-                if let Some(ref mut node) = $node.as_mut() {
-                    node.insert($value);
-                    return
-                }
+    ($node:expr, $value:expr) => {{
+        (move || {
+            if let Some(ref mut node) = $node.as_mut() {
+                node.insert($value);
+                return;
+            }
 
-                $node = Some(Box::new(Node::new($value)));
-            })()
-        }
-    };
+            $node = Some(Box::new(Node::new($value)));
+        })()
+    }};
 }
 
-impl Tree{
+impl Tree {
     fn new() -> Self {
-        Tree {
-            root: None,
-        }
+        Tree { root: None }
     }
 
     fn insert(&mut self, value: i32) {
@@ -37,7 +33,7 @@ impl Tree{
     }
 }
 
-impl Node{
+impl Node {
     fn new(value: i32) -> Self {
         Node {
             value: value,
@@ -55,39 +51,45 @@ impl Node{
     }
 }
 
-fn find_path (root: &Node, path: &mut Vec<i32>, value: i32) -> bool{
-    if root.value == 0 {return false;}
+fn find_path(root: Node, path: &mut Vec<i32>, value: i32) -> bool {
+    if root.value == 0 {
+        return false;
+    }
 
     path.push(root.value);
 
-    if root.value == value {return true;}
+    if root.value == value {
+        return true;
+    }
 
-    if root.left.is_some() && root.right.is_some(){
-        let left = *root.left.unwrap();
-        let right = *root.right.unwrap();
-
-    if (root.left.is_some() && find_path(&left, path, value)) ||
-    (root.right.is_some() && find_path(&right, path, value)){
-         return true;}
+    if (root.left.is_some() && find_path(*root.left.unwrap(), path, value))
+        || (root.right.is_some() && find_path(*root.right.unwrap(), path, value))
+    {
+        return true;
+    }
 
     path.pop();
-    return false;
-    }
+
     return false;
 }
 
-fn find_lca(root: Node, n1: Node, n2: Node) -> i32{
+fn find_lca(root: Node, n1: Node, n2: Node) -> i32 {
     let mut path1 = Vec::new();
     let mut path2 = Vec::new();
 
-    if !find_path(&root, &mut path1, n1.value) || !find_path(&root, &mut path2, n2.value){
-        return -1;}
+    let root1 = root.clone();
+    let root2 = root.clone();
+    if !find_path(root1, &mut path1, n1.value) || !find_path(root2, &mut path2, n2.value) {
+        return -1;
+    }
 
     let mut i = 0;
-    while i < path1.len() && i < path2.len(){
-        if path1[i] != path2[i] {break;}
+    while i < path1.len() && i < path2.len() {
+        if path1[i] != path2[i] {
+            break;
+        }
         i += 1;
-    return path1[i-1];
+        return path1[i - 1];
     }
     return -1;
 }
